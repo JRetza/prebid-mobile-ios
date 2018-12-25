ee7b6290937f1b0eba54a323012353d58c790f5f
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2018-12-25 16:26:46 +0100

    sample app

---
+++ sampleApp/AdManagerBannerExample/ViewController.h
@@ -0,0 +1,14 @@
+//  Copyright (c) 2014 Google. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@class DFPBannerView;
+@import GoogleMobileAds;
+
+@interface ViewController : UIViewController <GADAppEventDelegate>
+
+
+/// The DFP banner view.
+@property(nonatomic, weak) IBOutlet DFPBannerView *bannerView;
+
+@end
--- sdk/PrebidServerAdapter/PrebidServerModule/PBServerRequestBuilder.m
+++ sdk/PrebidServerAdapter/PrebidServerModule/PBServerRequestBuilder.m
@@ -71,7 +71,7 @@ static NSString *const kPrebidMobileVersion = @"0.5.2";
     requestDict[@"imp"] = [self openrtbImpsFromAdUnits:adUnits withSecureSettings:isSecure];
     requestDict[@"ext"] = [self openrtbRequestExtension:accountID];
     
-    //requestDict[@"test"] = 1;
+    requestDict[@"test"] = @(1);
     // requestDict[@"randomkey"] = @"234124123";
     
     return [requestDict copy];
